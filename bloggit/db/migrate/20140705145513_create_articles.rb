ea694f564530078c5194f39fd122c683a66c3ac3
FelixFortis <adamwenham64@gmail.com>
2014-07-05 16:22:04 +0000

    added login required to rticles actions

--- bloggit/db/migrate/20140705145513_create_articles.rb
+++ bloggit/db/migrate/20140705145513_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
       t.string :name
-      t.string :body
+        t.text :body
 
       t.timestamps
     end
