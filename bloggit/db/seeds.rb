687df39e3fc17b6a6278ac7083d1dc8ebd04b056
FelixFortis <adamwenham64@gmail.com>
2014-07-15 07:50:10 +0000

    more style amends to article forms

--- bloggit/db/seeds.rb
+++ bloggit/db/seeds.rb
@@ -5,21 +5,96 @@ Article.create(slug: 'markdown-test',
     title: 'Markdown converter test article',
     body: 
     %{
-# Hello There
-This is [an example](http://example.com/ "I'm the title") of an inline link.
-    
-### Define a list below
-    
-+ Get milk
-+ Buy food
-    
-This is paragraph text with some **bold** or some _italics_ how about _**both**_ ?
+# Header 1 #
+## Header 2 ##
+### Header 3 ###             (Hashes on right are optional)
+#### Header 4 ####
+##### Header 5 #####
 
-**Code** block below:
+## Markdown plus h2 with a custom ID ##        #id-goes-here
+[Link back to H2](#id-goes-here)
+
+This is a paragraph, which is text surrounded by whitespace. Paragraphs can be on one 
+line (or many), and can drone on for hours.  
+
+Here is a Markdown link to [Warped](http://warpedvisions.org), and a literal . 
+Now some SimpleLinks, like one to [google] (automagically links to are-you-
+feeling-lucky), a [wiki: test] link to a Wikipedia page, and a link to 
+[foldoc: CPU]s at foldoc.  
+
+Now some inline markup like _italics_,  **bold**, and `code()`. Note that underscores in 
+words are ignored in Markdown Extra.
+
+![picture alt](/images/photo.jpeg "Title is optional")     
+
+> Blockquotes are like quoted text in email replies
+>> And, they can be nested
+
+* Bullet lists are easy too
+- Another one
++ Another one
+
+1. A numbered list
+2. Which is numbered
+3. With periods and a space
+
+And now some code:
+
+    // Code is just text indented a bit
+    which(is_easy) to_remember();
+
+~~~
+
+// Markdown extra adds un-indented code blocks too
+
+if (this_is_more_code == true && !indented) 
+    // tild wrapped code blocks, also not indented
+
+
+~~~
+
+Text with  
+two trailing spaces  
+(on the right)  
+can be used  
+for things like poems  
+
+### Horizontal rules
+
+* * * *
+****
+--------------------------
+
+<div class="custom-class" markdown="1">
+This is a div wrapping some Markdown plus.  Without the DIV attribute, it ignores the 
+block. 
+</div>
+
+## Markdown plus tables ##
+
+| Header | Header | Right  |
+| ------ | ------ | -----: |
+|  Cell  |  Cell  |   $10  |
+|  Cell  |  Cell  |   $20  |
+
+* Outer pipes on tables are optional
+* Colon used for alignment (right versus left)
+
+## Markdown plus definition lists ##
+
+Bottled water
+: $ 1.25
+: $ 1.55 (Large)
+
+Milk
+Pop
+: $ 1.75
+
+* Multiple definitions and terms are possible
+* Definitions can include multiple paragraphs too
+
+*[ABBR]: Markdown plus abbreviations (produces an <abbr> tag)
     
-    function hello() { 
-      alert('Hello world!'); 
-    }
     })
 Article.create(slug: 'criminal-prosecuted-to-fullest-extent-of-budget',
     title: 'Criminal Prosecuted To Fullest Extent Of Budget',
